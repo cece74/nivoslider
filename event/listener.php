2f16f939ca231c33e61a45e85919119f92cdd752
cece74 <forumacquariofilia@libero.it>
2015-03-13 22:47:58 +0100

    Update listener.php

--- event/listener.php
+++ event/listener.php
@@ -20,17 +20,46 @@ class listener implements EventSubscriberInterface
 	static public function getSubscribedEvents()
 	{
 		return array(
-		'core.user_setup' => 'load_language_on_setup',
+			'core.user_setup'	=> 'load_language_on_setup',
+			'core.page_header'	=> 'add_page_header_slider',
 		);
 	}
 
-		public function load_language_on_setup($event)
+	/**
+	* Constructor
+	*/
+	public function __construct(\phpbb\template\template $template, \phpbb\db\driver\driver_interface $db, $table_prefix, $phpbb_root_path)
+	{
+		$this->template = $template;
+		$this->phpbb_root_path = $phpbb_root_path;
+		$this->db = $db;
+		$this->table_prefix = $table_prefix;
+	}
+
+	public function load_language_on_setup($event)
 	{
 		$lang_set_ext = $event['lang_set_ext'];
 		$lang_set_ext[] = array(
-			'ext_name' => 'cece74/nivoslider',
-			'lang_set' => 'common',
+			'ext_name'	=> 'cece74/nivoslider',
+			'lang_set'	=> 'common',
 		);
 		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
+	public function add_page_header_slider($event)
+	{
+		define ('SLIDER_TABLE', $this->table_prefix.'nivo_slider');
+
+		$sql = 'SELECT *
+			FROM ' . SLIDER_TABLE . '
+			ORDER BY order_img';
+		$result = $this->db->sql_query($sql);
+		while ($row = $this->db->sql_fetchrow($result))
+		{
+			$this->template->assign_block_vars('imgs', array(
+				'SLIDE_IMG'		=> '<img src="'. $this->phpbb_root_path .'ext/cece74/nivoslider/styles/all/theme/images/'. $row['file_name'] .'" alt="'. $row['img_alt'] .'" title="'. $row['img_title'] .'" />',
+			));
+		}
+		$this->db->sql_freeresult($result);
 	}
 }
