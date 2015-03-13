d9251a0c84fcdf9cfd134f8fb2e26f1cd75b689b
cece74 <forumacquariofilia@libero.it>
2015-03-13 22:49:24 +0100

    Create info_acp_nivo_slider.php

---
+++ language/en/info_acp_nivo_slider.php
@@ -0,0 +1,52 @@
+<?php
+/**
+*
+* Nivo Slider [Russian]
+*
+* @package Nivo Slider
+* @copyright (c) 2013 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+/**
+* @ignore
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'ACP_NIVO_SLIDER'					=> 'Slider',
+	'ACP_NIVO_SLIDER_MANAGE'			=> 'Management',
+	'ACP_NIVO_SLIDER_MANAGE_EXPLAIN'	=> 'Here you can download the images for the slider.',
+	'UPLOAD'							=> 'Upload file',
+	'FILE_NAME'							=> 'File name',
+	'SELECT_FILE'						=> 'Upload a file from your computer',
+	'UPLOAD_SUCCESS'					=> 'The %s has been successfully loaded.',
+	'UPLOAD_FILURE'						=> 'Could not load file %s.',
+	'FILE_ALREADY_EXISTS'				=> 'Sorry, the file already exists.',
+	'FILE_TOO_LARGE'					=> 'File size exceeds the maximum.',
+	'ILLEGAL_FILE_TYPE'					=> 'Invalid file type, only files types JPG, JPEG, PNG or GIF.',
+	'IMAGE_ONLY'						=> 'Illegal type: file is not an image.',
+	'DELETE_SUCCESS'					=> 'File %s was removed successfully.',
+	'DELETE_ERROR'						=> 'Failed to delete file.',
+));
