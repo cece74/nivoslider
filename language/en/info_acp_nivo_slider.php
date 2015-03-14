bb250b9b0cf27898923ac3e64476d31e46d7cc70
AlexSheer <alex_sheer@inbox.lv>
2015-03-14 20:44:42 +0200

    Update info_acp_nivo_slider.php

--- language/en/info_acp_nivo_slider.php
+++ language/en/info_acp_nivo_slider.php
@@ -1,7 +1,7 @@
 <?php
 /**
 *
-* Nivo Slider [Russian]
+* Nivo Slider [English]
 *
 * @package Nivo Slider
 * @copyright (c) 2013 phpBB Group
@@ -49,4 +49,7 @@ $lang = array_merge($lang, array(
 	'IMAGE_ONLY'						=> 'Illegal type: file is not an image.',
 	'DELETE_SUCCESS'					=> 'File %s was removed successfully.',
 	'DELETE_ERROR'						=> 'Failed to delete file.',
+	'URL'								=> 'Link',
+	'ILLEGAL_URL'						=> 'Wrong URL',
+	'NO_IMAGE'							=> 'File not selected.',
 ));
